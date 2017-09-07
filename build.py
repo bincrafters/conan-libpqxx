bac37a2f6e5e34f944fa9977aca9c447487997f6
Javier G. Sogo <jgsogo@gmail.com>
2017-09-07 21:25:24 +0200

    missing line

--- build.py
+++ build.py
@@ -3,6 +3,7 @@ import os, platform
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(stable_branch_pattern='master')
+    builder.add_common_builds()
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
         if settings["arch"] == "x86_64" and settings["build_type"] == "Release":
