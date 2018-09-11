b984924a15a3a5f32dc144b973107707215b2007
Uilian Ries <uilianries@gmail.com>
2018-09-10 22:55:17 -0300

    Build using CMake
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
-import shutil
-from conans import ConanFile, AutoToolsBuildEnvironment, tools, VisualStudioBuildEnvironment
+from conans import ConanFile, tools, CMake
 
 
 class LibpqxxRecipe(ConanFile):
@@ -15,84 +14,36 @@ class LibpqxxRecipe(ConanFile):
     homepage = "https://github.com/jtv/libpqxx"
     author = "Bincrafters <bincrafters@gmail.com>"
     license = "BSD-3"
+    generators = "cmake"
+    exports = "LICENSE.md"
+    exports_sources = "CMakeLists.txt"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = "shared=False", "fPIC=True"
     requires = "libpq/9.6.9@bincrafters/stable"
     source_subfolder = "source_subfolder"
-    autotools = None
 
     def config_options(self):
         if self.settings.os == "Windows":
             self.options.remove("fPIC")
-
-    def configure(self):
-        if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
-            if "MT" in self.settings.compiler.runtime:
-                raise Exception("libpqxx does not support MT")
 
     def source(self):
         tools.get("{0}/archive/{1}.tar.gz".format(self.homepage, self.version))
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self.source_subfolder)
 
-    def configure_autotools(self):
-        if not self.autotools:
-            self.autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
-            args = ["--disable-documentation"]
-            args.append("--with-postgres-include={}".format(os.path.join(self.deps_cpp_info["libpq"].rootpath, "include")))
-            args.append("--with-postgres-lib={}".format(os.path.join(self.deps_cpp_info["libpq"].rootpath, "lib")))
-            with tools.chdir(self.source_subfolder):
-                self.autotools.configure(args=args)
-        return self.autotools
-
-    def windows_build(self):
-        # Follow instructions in https://github.com/jtv/libpqxx/blob/master/win32/INSTALL.txt
-        common_file = os.path.join(self.source_subfolder, 'win32', 'common')
-        with open(common_file, "w") as fd:
-            postgres_path = self.deps_cpp_info["libpq"].rootpath
-            fd.write(b'PGSQLSRC="{}"\n'.format(postgres_path))
-            fd.write(b'PGSQLINC="{}"\n'.format(os.path.join(postgres_path, "include")))
-            fd.write(b'LIBPQINC="{}"\n'.format(os.path.join(postgres_path, "include")))
-
-        target_dir = os.path.abspath(os.path.join(self.source_subfolder, 'include', 'pqxx'))
-        with tools.chdir(os.path.join(self.source_subfolder, 'config', 'sample-headers', 'compiler', 'VisualStudio2013', 'pqxx')):
-            shutil.copy('config-internal-compiler.h', target_dir)
-            shutil.copy('config-public-compiler.h', target_dir)
-
-        vcvars = tools.vcvars_command(self.settings)
-        self.run(vcvars)
-        env = VisualStudioBuildEnvironment(self)
-        with tools.environment_append(env.vars):
-            with tools.chdir(self.source_subfolder):
-                target = "DLL" if self.options.shared else "STATIC"
-                target += str(self.settings.build_type).upper()
-                command = 'nmake /f win32/vc-libpqxx.mak %s' % target
-                self.run(command)
-
-    def unix_build(self):
-        autotools = self.configure_autotools()
-        with tools.chdir(self.source_subfolder):
-            autotools.make()
+    def configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure()
+        return cmake
 
     def build(self):
-        if self.settings.os == "Linux" or self.settings.os == "Macos":
-            self.unix_build()
-        elif self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
-            self.windows_build()
-        else:
-            raise Exception("Could not build: Platform {} is not supported.".format(self.settings.os))
+        cmake = self.configure_cmake()
+        cmake.build()
 
     def package(self):
-        self.copy(pattern="COPYING", dst="licenses", src=self.source_subfolder)
-        if self.settings.os == "Linux" or self.settings.os == "Macos":
-            autotools = self.configure_autotools()
-            with tools.chdir(self.source_subfolder):
-                autotools.install()
-        elif self.settings.os == "Windows":
-            self.copy("*", dst="include", src=os.path.join(self.source_subfolder, "include"))
-            self.copy("*.h", dst="include", src=os.path.join(self.source_subfolder, "include"))
-            self.copy("*.lib", dst="lib", src=os.path.join(self.source_subfolder, "lib"))
-            self.copy("*.dll", dst="bin", src=os.path.join(self.source_subfolder, "lib"))
+        self.copy("COPYING", dst="licenses", src=self.source_subfolder)
+        cmake = self.configure_cmake()
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
--- test_package/main.cpp
+++ test_package/main.cpp
@@ -6,5 +6,12 @@
 int main()
 {
     std::cout << "libpqxx version: " << PQXX_VERSION << std::endl;
+
+    pqxx::connection conn("postgres://tpilupsd:7hJFAnmfyuD27orImLVfKQX1mdMUJTq_@stampy.db.elephantsql.com:5432/tpilupsd");
+    std::cout << "is connected: "<< conn.is_open() << std::endl;
+    if (conn.is_open()) {
+        conn.disconnect();
+    }
+
     return EXIT_SUCCESS;
 }
