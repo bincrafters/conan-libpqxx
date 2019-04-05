00d40f1ec8d99e76c511dcfd209884bafb71aab3
tt4g <mainlinett@gmail.com>
2019-04-05 20:25:30 +0900

    Bump patch version to 6.4.3

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ from conans.errors import ConanInvalidConfiguration
 
 class LibpqxxRecipe(ConanFile):
     name = "libpqxx"
-    version = "6.4.2"
+    version = "6.4.3"
     settings = "os", "compiler", "build_type", "arch", "cppstd"
     description = "The official C++ client API for PostgreSQL"
     url = "https://github.com/bincrafters/conan-libpqxx"
@@ -46,7 +46,7 @@ class LibpqxxRecipe(ConanFile):
                                              " could not be built by apple-clang < 8.0"))
 
     def source(self):
-        sha256 = "f3afb60b8f6d69a8077f7e7f30fc7175ce7d437551d386dba4f7e0f1e7b673ea"
+        sha256 = "0c70d6986a93f07876f173cfc606f9ced2711bcfbd53e59842e682d48a281138"
         tools.get("{0}/archive/{1}.tar.gz".format(self.homepage, self.version), sha256=sha256)
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
